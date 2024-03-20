c6f1d9759a9a986696daba30a575054f606e6a11
johnpathapati <johnpathapati@gmail.com>
2024-03-21 00:01:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import BasicClassComponent from './Component/basic-class-component';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>This is my First Application</h1>
+
+      <BasicClassComponent></BasicClassComponent>
+    </div>
+  );
+ 
+}
+
+export default App;
